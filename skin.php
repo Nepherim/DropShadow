f2fac1e2346cb08a84c89973137c8b56fc89d4ad
David Gilbert <david@solidgone.com>
2009-02-25 03:55:42 +0000

    new: Added #subcontent styling (for sidebar list icons). new: Added styling for readmore, fieldset, wikimessage. new: Added fieldset to searchbox for xhtml compliance. new: Support for blogger (pmmarkup, itemsonpage, perpage, link to comment author website chg: Standardized tabs, navigation, footer naming, and search order. chg: Formated skin.php to standard skin coding (colors, linkfmt). chg: Removed header automatic decrease (from ! to !!, etc). chg: Link to stylesheet rather than import. fix: Make header text link to root, using $WikiTitle. fix: Standardized skin identification text in footer.

--- skin.php
+++ skin.php
@@ -1,29 +1,17 @@
 <?php if (!defined('PmWiki')) exit();
-/*
- * PmWiki DropShadow skin
- * @requires PmWiki 2.2
+/* PmWiki DropShadow skin
  *
- * Examples at: http://pmwiki.com/Cookbook/DropShadow and http://skins.solidgone.com/
- * Copyright (c) 2007 David Gilbert
+ * Examples at: http://pmwiki.com/Cookbook/DropShadow and http://solidgone.com/Skins/
+ * Copyright (c) 2009 David Gilbert
  * Dual licensed under the MIT and GPL licenses:
  *    http://www.opensource.org/licenses/mit-license.php
  *    http://www.gnu.org/licenses/gpl.html
  */
 global $FmtPV;
-$FmtPV['$SkinName'] = '"dropshadow"';
-$FmtPV['$SkinVersion'] = '"0.1.0"';
+$FmtPV['$SkinName'] = '"DropShadow"';
+$FmtPV['$SkinVersion'] = '"0.2.0"';
 
-## Default color scheme
-global $SkinColor, $ValidSkinColors;
-if ( !is_array($ValidSkinColors) ) $ValidSkinColors = array();
-array_push($ValidSkinColors, 'red', 'blue', 'orange', 'yellow', 'green');
-if ( isset($_GET['color']) && in_array($_GET['color'], $ValidSkinColors) ) {
-	$SkinColor = $_GET['color'];
-} elseif ( !in_array($SkinColor, $ValidSkinColors) ) {
-	$SkinColor = 'blue';
-}
-
-## Move any (:noleft:) or SetTmplDisplay('PageLeftFmt', 0); directives to variables for access in jScript.
+# Move any (:noleft:) or SetTmplDisplay('PageLeftFmt', 0); directives to variables for access in jScript.
 $FmtPV['$RightColumn'] = "\$GLOBALS['TmplDisplay']['PageRightFmt']";
 Markup('noright', 'directives',  '/\\(:noright:\\)/ei', "SetTmplDisplay('PageRightFmt',0)");
 $FmtPV['$SearchBar'] = "\$GLOBALS['TmplDisplay']['PageSearchFmt']";
@@ -33,23 +21,32 @@ Markup('noaction', 'directives',  '/\\(:noaction:\\)/ei', "SetTmplDisplay('PageA
 $FmtPV['$TabsBar'] = "\$GLOBALS['TmplDisplay']['PageTabsFmt']";
 Markup('notabs', 'directives',  '/\\(:notabs:\\)/ei', "SetTmplDisplay('PageTabsFmt',0)");
 
-## Add a custom page storage location
-global $PageStorePath, $WikiLibDirs;
+# ----------------------------------------
+# - Standard Skin Setup
+# ----------------------------------------
+$FmtPV['$WikiTitle'] = '$GLOBALS["WikiTitle"]';
+
+# Define a link stye for new page links
+global $LinkPageCreateFmt;
+SDV($LinkPageCreateFmt, "<a class='createlinktext' href='\$PageUrl?action=edit'>\$LinkText</a>");
+
+# Default color scheme
+global $SkinColor, $ValidSkinColors;
+if ( !is_array($ValidSkinColors) ) $ValidSkinColors = array();
+array_push($ValidSkinColors, 'red', 'blue', 'orange', 'yellow', 'green');
+if ( isset($_GET['color']) && in_array($_GET['color'], $ValidSkinColors) ) {
+	$SkinColor = $_GET['color'];
+} elseif ( !in_array($SkinColor, $ValidSkinColors) ) {
+	$SkinColor = 'blue';
+}
+
+# Override pmwiki styles otherwise they will override styles declared in css
+global $HTMLStylesFmt;
+$HTMLStylesFmt['pmwiki'] = '';
+
+# Add a custom page storage location
+global $WikiLibDirs;
 $PageStorePath = dirname(__FILE__)."/wikilib.d/{\$FullName}";
 $where = count($WikiLibDirs);
 if ($where>1) $where--;
 array_splice($WikiLibDirs, $where, 0, array(new PageStore($PageStorePath)));
-
-## Override pmwiki styles otherwise they will override styles declared in css
-global $HTMLStylesFmt;
-$HTMLStylesFmt['pmwiki'] = '';
-
-## Define a link stye for new page links
-global $LinkPageCreateFmt;
-SDV($LinkPageCreateFmt, "<a class='createlinktext' href='\$PageUrl?action=edit'>\$LinkText</a>");
-
-# Converts H1 to H2, H2 to H3, etc.
-Markup('^!', 'block',
-  '/^(!{1,5})\\s?(.*)$/e',
-  "'<:block,1><h'.(strlen('$1')+1).PSS('>$2</h').(strlen('$1')+1).'>'"
-);
